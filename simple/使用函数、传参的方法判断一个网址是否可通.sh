a6d674b3f3092fba66a4e6cd34e062411eaa3408
lifa1225 <991179382@qq.com>
2018-12-20 16:57:35 +0800

    update simple script

--- simple/使用函数、传参的方法判断一个网址是否可通.sh
+++ simple/使用函数、传参的方法判断一个网址是否可通.sh
@@ -3,23 +3,24 @@
 web_check(){
 num=`curl -I -s -w "%{http_code}\n" $1 | head -1 | grep "\b200\b" | wc -l`
 if [ $num -eq 1 ];then
-  echo "web is ok"
-  return 0
+    echo "web is ok"
+    return 0
 else
-  echo "not ok"
-  return 1
+    echo "not ok"
+    return 1
 fi
 }
+
 while true
 do
-clear
-read -p "please input an url:" url
-if [ ! -n "$url" ];then
-  echo "you must input an url!"
-  sleep 2
   clear
-else
-  web_check $url
-  exit 0
-fi
+  read -p "please input an url:" url
+  if [ ! -n "$url" ];then
+      echo "you must input an url!"
+      sleep 2
+      clear
+  else
+      web_check $url
+      exit 0
+  fi
 done
